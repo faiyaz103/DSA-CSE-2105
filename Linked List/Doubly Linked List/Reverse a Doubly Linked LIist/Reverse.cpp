4ce909cf006c4010b56598ed5335dbae43568268
Faiyaz Mahmud <123292492+faiyaz103@users.noreply.github.com>
2023-05-05 15:11:27 +0600

    Create Reverse.cpp

---
+++ Linked List/Doubly Linked List/Reverse a Doubly Linked LIist/Reverse.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node *next,*prev;
+};
+
+
+
+/*----------Creation----------*/
+void creation(node **head, int data){
+
+    node *ptr=new node();
+
+    ptr->data=data;
+    ptr->next=NULL;
+    ptr->prev=NULL;
+
+    if(*head==NULL){
+        *head=ptr;
+    }
+    else{
+        node *temp=*head;
+
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next=ptr;
+        ptr->prev=temp;
+    }
+}
+/*----------------------------*/
+
+
+
+/*-----------Reverse-----------*/
+void rev(node **head){
+
+    node *temp=*head,*proxy;
+    while(1){
+        proxy=temp->next;
+        temp->next=temp->prev;
+        temp->prev=proxy;
+
+        if(temp->prev==NULL){
+            *head=temp;
+            break;
+        }
+        else{
+            temp=temp->prev;
+        }
+    }
+
+    cout<<"Reverse operation executed"<<endl;
+}
+/*-----------------------------*/
+
+
+
+/*-----------Display----------*/
+void display(node **head){
+
+    node *temp=*head;
+
+    while(temp!=NULL){
+        cout<<temp->data<<' ';
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+/*----------------------------*/
+
+
+
+int main(){
+
+    int n,data;
+    node *head=NULL;
+
+    cout<<"Enter number of elements: ";
+    cin>>n;
+    cout<<endl;
+
+    for(int i=1; i<=n; i++){
+        cout<<"Enter "<<i<<"th element: ";
+        cin>>data;
+        cout<<endl;
+
+        creation(&head,data);
+    }
+
+    cout<<"The elements are:"<<endl;
+    display(&head);
+
+    rev(&head);
+
+    cout<<"The elements are:"<<endl;
+    display(&head);
+
+    return 0;
+}
